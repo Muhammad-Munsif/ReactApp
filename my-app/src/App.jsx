fdea2f87a22d2d03ce16f889078205ca3e05397f
MUHAMMAD-MUNSIF <m.munsif4375@gmail.com>
2025-07-04 15:26:06 +0500

    small change in coding.

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -11,8 +11,8 @@ function App() {
   return (
     <>
     <ControlledComponent />
-    <SpreadOpertor />
-    <SpreadObject />
+    {/* <SpreadOpertor /> */}
+    {/* <SpreadObject /> */}
     </>
   )
 }
